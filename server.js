6a88747bca1092ecdc79604116e69d5ac3f796ee
Mateo FD <yo@mail.com>
2020-01-17 14:24:44 +0100

    Añadido modelo, las rutas y el controller

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const apiRoutes = require('./routes.js')
 
 mongoose.connect("mongodb://localhost:27017/tienda", { useNewUrlParser: true })
 .then( db => console.log("Conexión a BD correcta") )
 .catch( error => console.log("Error al conectarse a la BD" + error));
 
-
 const app = express();
 
-app.get("/",(req, res)=> { 
-    res.send("<h1>Hola Mundo</h1>") 
-});
-
-
-app.get("/hola",(req, res) => 
-res.send("hola hola")
-);
-
 app.listen(3000, () => console.log("Servidor iniciado..."));
+
+app.use('/api', apiRoutes);
+app.use(express.json())
